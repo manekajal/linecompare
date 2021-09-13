56ba31410ee0c8bfbecab411419e87fe1cf21e38
kajal <manekajal2014@gmail.com>
2021-09-13 14:44:31 +0530

    Add Welcome to Line Comparison

---
+++ src/main/java/bridgelabz/LineCompare.java
@@ -0,0 +1,8 @@
+package bridgelabz;
+
+public class LineCompare {
+    public static void main(String args[]) {
+
+        System.out.println("Welcome to Line Comparison Computation Program on Master Branch");
+    }
+}
